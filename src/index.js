be881e43ba0d1ca432bc1ce373d3b79feb4ec9aa
afeiship <1290657123@qq.com>
2021-10-18 09:57:03 +0800

    fix: err parse

--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-
-  var nxFileUpload = require('@jswork/next-file-upload');
-  var NxWeiboToPics = require('@jswork/next-weibo-to-pics');
+  var nxFileUpload = nx.fileUpload || require('@jswork/next-file-upload');
+  var NxWeiboToPics = nx.weiboToPics || require('@jswork/next-weibo-to-pics');
   var WEIBO_API = '/weibo_api/interface/pic_upload.php';
   var WEIBO_IMG = 'https://tva1.sinaimg.cn';
   var ROOT_COOKIE = '; Path=/;';
 
   var NxWeiboOss = nx.declare('nx.WeiboOss', {
     methods: {
-      init: function (inToken, inOptions) {
-        this._token = inToken;
-        this._options = inOptions;
-        this.initToken();
-      },
-      initToken: function () {
-        var subCookie = this._token;
-        if (typeof this._token !== 'string') {
-          subCookie = this._token.find(function (item) {
-            return item.indexOf('SUB=') > -1;
-          });
-        }
-        // SUB=_2A252mjcRDeRhGeBO7lAX8CzOyjSIHXVV7i_ZrDV8PUNbmtBeLVejkW9NReLbikXG3fXjOzUGTCZi9E5emuh2wIbb; Path=/;
-        global.document.cookie = subCookie.split(';')[0] + ROOT_COOKIE;
+      init: function (inToken) {
+        global.document.cookie = 'SUB=' + inToken + ROOT_COOKIE;
       },
       process: function (inResponse, inResolve, inReject) {
         var _response = JSON.parse(inResponse.split('\n')[2]);
         var data = _response.data;
         if (data.count > 0) {
-          var pics = NxWeiboToPics(data.pics);
+          var pics = NxWeiboToPics(inResponse);
           var value = pics.map(function (item) {
             item.type = item.pid.charAt(21) === 'g' ? 'gif' : 'jpg';
             item.url = [WEIBO_IMG, '/large/', item.pid, '.', item.type].join('');
