7046f1c0b4a57efcea9e8699b34665d74b90217a
afeiship <1290657123@qq.com>
2021-10-19 09:22:03 +0800

    refactor: simplify code

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  * description: Weibo oss for next api.
  * homepage: https://github.com/afeiship/next-weibo-oss
  * version: 1.0.1
- * date: 2021-10-18 09:58:57
+ * date: 2021-10-19 09:21:54
  * license: MIT
  */
 
@@ -11,9 +11,8 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
   var nxFileUpload = nx.fileUpload || require('@jswork/next-file-upload');
-  var NxWeiboToPics = nx.weiboToPics || require('@jswork/next-weibo-to-pics');
+  var nxWeiboToPics = nx.weiboToPics || require('@jswork/next-weibo-to-pics');
   var WEIBO_API = '/weibo_api/interface/pic_upload.php';
-  var WEIBO_IMG = 'https://tva1.sinaimg.cn';
   var ROOT_COOKIE = '; Path=/;';
 
   var NxWeiboOss = nx.declare('nx.WeiboOss', {
@@ -24,17 +23,7 @@
       process: function (inResponse, inResolve, inReject) {
         var _response = JSON.parse(inResponse.split('\n')[2]);
         var data = _response.data;
-        if (data.count > 0) {
-          var pics = NxWeiboToPics(inResponse);
-          var value = pics.map(function (item) {
-            item.type = item.pid.charAt(21) === 'g' ? 'gif' : 'jpg';
-            item.url = [WEIBO_IMG, '/large/', item.pid, '.', item.type].join('');
-            return item;
-          });
-          inResolve(value);
-        } else {
-          inReject(inResponse);
-        }
+        data.count > 0 ? inResolve(nxWeiboToPics(inResponse)) : inReject(inResponse);
       },
       upload: function (inFile) {
         var self = this;
